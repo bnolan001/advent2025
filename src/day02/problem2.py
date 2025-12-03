afb3208e54ef670fd560ebb6b3abb763a8e4bd89
Brian Nolan <bnolan001@gmail.com>
2025-12-02 19:08:14 -0500

    Completed day 2 part 2

--- src/day02/problem2.py
+++ src/day02/problem2.py
@@ -1,7 +1,41 @@
 import os
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-path = os.path.join(script_dir, 'sample.txt')
-with open(path, 'r') as file:
 
-    
+def find_repeats(start, end):
+    repeats = []
+    for n in range(start, end + 1):
+        if n < 10:
+            continue
+        
+        n_str = f'{n}'
+        n_len = len(n_str)
+        halfway = int(n_len/2)
+        for h in range(halfway, 0, -1):
+
+            if n_len % h != 0:
+                continue
+            times = int(n_len / h)
+            sub_n = n_str[0:h]
+            sub_n_whole = sub_n * times
+
+            whole = int(sub_n_whole)
+
+            if whole == n:
+                repeats += [n]
+
+    return set(repeats)
+        
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(script_dir, 'input.txt')
+with open(path, 'r') as file:
+    result = 0
+    repeats = []
+    for line in file:
+        ranges = line.split(',')
+        for r in ranges:
+            start, end = r.split('-')
+            repeats += list(find_repeats(int(start), int(end)))
+
+    result = sum(repeats)
+    print(result)
