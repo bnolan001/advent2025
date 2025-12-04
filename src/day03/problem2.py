f0be2ac77f435e0671eec333a5f506a735167d1d
Brian Nolan <bnolan001@gmail.com>
2025-12-03 19:42:21 -0500

    Brute force no worky

--- src/day03/problem2.py
+++ src/day03/problem2.py
@@ -4,32 +4,32 @@ import os
 def get_largest_joltage(num_str):
     joltage = ["0"] * 12
     largest = ["0"] * 12
-    for a in range(len(num_str) - 12):
+    for a in range(len(num_str) - 11):
         joltage[0] = num_str[a]
-        for b in range(a + 1,len(num_str) - 11 - a):
+        for b in range(a + 1,len(num_str) - 10):
             joltage[1] = num_str[b]
-            for c in range(b + 1,len(num_str) - 10 - b):
+            for c in range(b + 1,len(num_str) - 9):
                 joltage[2] = num_str[c]
-                for d in range(c + 1,len(num_str) - 9 - c):
+                for d in range(c + 1,len(num_str) - 8):
                     joltage[3] = num_str[d]
-                    for e in range(d + 1,len(num_str) - 8 - d):
+                    for e in range(d + 1,len(num_str) - 7):
                         joltage[4] = num_str[e]
-                        for f in range(e + 1, len(num_str) - 7 - e):
+                        for f in range(e + 1, len(num_str) - 6):
                             joltage[5] = num_str[f]
-                            for g in range(f + 1,len(num_str) - 6 - f):
+                            for g in range(f + 1,len(num_str) - 5):
                                 joltage[6] = num_str[g]
-                                for h in range(g + 1,len(num_str) - 5 - g):
+                                for h in range(g + 1,len(num_str) - 4):
                                     joltage[7] = num_str[h]
-                                    for i in range(h + 1,len(num_str) - 4 - h):
+                                    for i in range(h + 1,len(num_str) - 3):
                                         joltage[8] = num_str[i]
-                                        for j in range(i + 1,len(num_str) - 3 - i):
+                                        for j in range(i + 1,len(num_str) - 2):
                                             joltage[9] = num_str[j]
-                                            for k in range(j + 1, len(num_str) - 2 - j):
+                                            for k in range(j + 1, len(num_str) - 1):
                                                 joltage[10] = num_str[k]
-                                                for l in range(k + 1,len(num_str) - 1 - k):
+                                                for l in range(k + 1,len(num_str)):
                                                     joltage[11] = num_str[l]
                                                     if joltage > largest:
-                                                        largest = joltage
+                                                        largest = list(joltage)
 
     print(largest)
     return int("".join(largest))
@@ -37,7 +37,7 @@ def get_largest_joltage(num_str):
         
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-path = os.path.join(script_dir, 'sample.txt')
+path = os.path.join(script_dir, 'input.txt')
 with open(path, 'r') as file:
     result = 0
     joltages = []
